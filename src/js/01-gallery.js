223c0a77f1a90971bd585895db325e9d0df7f1bb
Paweł Rosłaniec <pawel94roslaniec@gmail.com>
2022-06-07 13:57:32 +0200

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,5 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from '/gallery-items';
 // Change code below this line
 import SimpleLightbox from "simplelightbox";
 // Dodatkowy import stylów
