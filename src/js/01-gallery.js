c282e8b6f31453dd37c051483554b65a24d779cd
Paweł Rosłaniec <pawel94roslaniec@gmail.com>
2022-06-08 10:44:57 +0200

    edited gallery.js, added src

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,7 @@
 // Add imports above this line
-import { galleryItems } from '/gallery-items';
+import { galleryItems } from './gallery-items';
 // Change code below this line
-import SimpleLightbox from "simplelightbox";
+import SimpleLightbox from 'simplelightbox';
 // Dodatkowy import stylów
 import 'simplelightbox/dist/simple-lightbox.min.css';
 console.log(galleryItems);
